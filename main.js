d20def79a8c737dcc7d75985e60670fa735f3412
Jody Isaguirre <jodyisaguirre@Jodys-MacBook-Air.local>
2018-09-19 10:53:16 -0600

    removed background and changed grid

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 let container =document.querySelector(".container")
-
 let chosenColor = "";
-function handleClick(event){
 
+function handleClick(event){
   event.target.style.backgroundColor = chosenColor;
 }
 container.addEventListener("click", handleClick);
@@ -23,7 +22,7 @@ container.appendChild(tblB);
 for(var i=0; i<10; i++){
   var tr = document.createElement('tr');
   tblB.appendChild(tr);
-  
+
   for (var j = 0; j< 20; j++){
     var td = document.createElement('td')
     tr.appendChild(td);
